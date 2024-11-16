870ec7511e127db183aa6aa840f51ffd255896a8
Kyle Tran <kyle.tran@fssi.co.nz>
2024-11-16 22:51:33 +1300

    custom action with javascript

--- .github/custom_actions/deploy-s3-javascript/main.js
+++ .github/custom_actions/deploy-s3-javascript/main.js
@@ -4,6 +4,15 @@ const exec = require('@actions/exec');
 
 function run() {
     core.notice('Hello from my custom JavaScript Action')
+    // 1) Get some input values
+    const bucket = core.getInput('bucket');
+    const bucketRegion = core.getInput('bucket-region');
+    const distFolder = core.getInput('dist-folder');
 
+    // 2) Upload fukes
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice('Hellow from my custom Javascript Action');
 }
 run();
